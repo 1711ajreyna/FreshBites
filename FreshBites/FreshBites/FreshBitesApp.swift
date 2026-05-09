de3518ee7fdbf20907f026142154f434a12ac2e5
Andrew Reyna <andrewreyna@Mac.lan>
2026-05-08 20:44:32 -0700

    initial commit

---
+++ FreshBites/FreshBites/FreshBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshBitesApp.swift
+//  FreshBites
+//
+//  Created by Andrew Reyna on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FreshBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FreshBites/FreshBitesTests/FreshBitesTests.swift
@@ -0,0 +1,16 @@
+//
+//  FreshBitesTests.swift
+//  FreshBitesTests
+//
+//  Created by Andrew Reyna on 5/6/26.
+//
+
+import Testing
+
+struct FreshBitesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
